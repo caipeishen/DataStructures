0eff0a00c2bbe7e93067616c3a4024744fb74c70
Cai Peishen <peishen.cai@foxmail.com>
2021-06-01 14:08:13 +0800

    单项队列 双向队列

--- src/main/java/cn/cps/App.java
+++ src/main/java/cn/cps/App.java
@@ -1,13 +1,31 @@
 package cn.cps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // 空集合并不会报空指针异常
+
+        List<String> list = new ArrayList<>();
+
+        list.addAll(Stream.of("1","2","3").collect(Collectors.toList()));
+
+        List<String> collect = list.stream()
+                .filter(str -> str.equals("0"))
+                .filter(str -> str.equals(""))
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println(collect.toString());
     }
 }
---
+++ src/main/java/cn/cps/linkedlist/TestStack.java
@@ -0,0 +1,27 @@
+package cn.cps.linkedlist;
+
+import java.util.Stack;
+
+/**
+ * @Author: Cai Peishen
+ * @Date: 2021/6/1 11:11
+ * @Description:
+ */
+//演示栈Stack的基本使用
+public class TestStack {
+
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack();
+        // 入栈
+        stack.add("jack");
+        stack.add("tom");
+        stack.add("smith");
+
+        // 出栈
+        // smith, tom , jack
+        while (stack.size() > 0) {
+            System.out.println(stack.pop());//pop就是将栈顶的数据取出
+        }
+    }
+
+}
